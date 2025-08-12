a2284a370cd4a2f46f434c281587358d8d0b2f22
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:23:33 -0300

    Delete ex1_percorrer_lista.py

--- ex1_percorrer_lista.py
+++
@@ -1,6 +0,0 @@
-def percorrer_lista(lista):
-    for elemento in lista:
-        print(elemento)
-
-# Exemplo
-percorrer_lista( [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9])
