32443f22eafd390aedea4b72bb0a04f439546f73
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:24:45 -0300

    Delete ex11_lista_vazia.py

--- ex11_lista_vazia.py
+++
@@ -1,5 +0,0 @@
-def lista_vazia(lista):
-    return len(lista) == 0
-
-itens = input("Digite elementos separados por espaço (ou pressione Enter para lista vazia): ").split()
-print("A lista está vazia?", lista_vazia(itens))
