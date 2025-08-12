55cebb7839a7982d49148f9d328f106278a2952a
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:22:40 -0300

    Delete ex04_numeros_pares.py

--- ex04_numeros_pares.py
+++
@@ -1,5 +0,0 @@
-def numeros_pares(lista):
-    return [n for n in lista if n % 2 == 0]
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-print("Números pares:", numeros_pares(numeros))
