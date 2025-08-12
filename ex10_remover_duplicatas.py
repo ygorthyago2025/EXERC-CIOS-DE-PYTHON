7f730cda16bb02eb803da26e78a713e99284032e
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:24:07 -0300

    Delete ex10_remover_duplicatas.py

--- ex10_remover_duplicatas.py
+++
@@ -1,5 +0,0 @@
-def remover_duplicatas(lista):
-    return list(set(lista))
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-print("Lista sem duplicatas:", remover_duplicatas(numeros))
