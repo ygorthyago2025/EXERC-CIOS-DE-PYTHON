450e5c8654595002a2b1fc4cd274467abdca9bdb
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:22:09 -0300

    Delete ex05_numeros_impares.py

--- ex05_numeros_impares.py
+++
@@ -1,5 +0,0 @@
-def numeros_impares(lista):
-    return [n for n in lista if n % 2 != 0]
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-print("Números ímpares:", numeros_impares(numeros))
