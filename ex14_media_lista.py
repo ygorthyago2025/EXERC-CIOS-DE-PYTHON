66a1a2c2ac7af43b54b72246a055c04921937503
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:23:51 -0300

    Delete ex14_media_lista.py

--- ex14_media_lista.py
+++
@@ -1,5 +0,0 @@
-def media_lista(lista):
-    return sum(lista) / len(lista) if lista else 0
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-print("Média:", media_lista(numeros))
