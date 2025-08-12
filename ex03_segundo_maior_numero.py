556fc9546daa72a75c363acd42fa76ff16b41906
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:22:28 -0300

    Delete ex03_segundo_maior_numero.py

--- ex03_segundo_maior_numero.py
+++
@@ -1,5 +0,0 @@
-def segundo_maior(lista):
-    return sorted(set(lista))[-2]
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-print("Segundo maior número:", segundo_maior(numeros))
