ced245f6c5f43d1b51f99f781d9699baa58d1e91
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-08-12 19:24:27 -0300

    Delete ex06_buscar_numero.py

--- ex06_buscar_numero.py
+++
@@ -1,6 +0,0 @@
-def buscar_numero(numero, lista):
-    return numero in lista
-
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
-alvo = int(input("Digite o número para buscar: "))
-print("Encontrado?", buscar_numero(alvo, numeros))
