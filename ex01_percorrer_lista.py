032a49715ec9e77752ade593fa017e918d83ef3c
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:51:28 -0300

    Update ex01_percorrer_lista.py

--- ex01_percorrer_lista.py
+++ ex01_percorrer_lista.py
@@ -2,6 +2,8 @@ def percorrer_lista(lista):
     for elemento in lista:
         print(elemento)
 
-# Entrada do usuário
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 percorrer_lista(numeros)
