be18067fb48075e7a69eddfb18f91b906e50b780
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:57:02 -0300

    Update ex08_soma_lista.py

--- ex08_soma_lista.py
+++ ex08_soma_lista.py
@@ -1,5 +1,10 @@
 def soma_lista(lista):
     return sum(lista)
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 print("Soma dos números:", soma_lista(numeros))
+
+
