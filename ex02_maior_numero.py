80c0da59c13a89aab2ef1708db310c9e46bdd094
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:53:14 -0300

    Update ex02_maior_numero.py

--- ex02_maior_numero.py
+++ ex02_maior_numero.py
@@ -1,5 +1,8 @@
 def maior_numero(lista):
     return max(lista)
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 print("Maior número:", maior_numero(numeros))
