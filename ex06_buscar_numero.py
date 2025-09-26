fac45e3e34359f32c1b374d4d02f61fdfd53a98a
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:56:14 -0300

    Update ex06_buscar_numero.py

--- ex06_buscar_numero.py
+++ ex06_buscar_numero.py
@@ -1,6 +1,9 @@
 def buscar_numero(numero, lista):
     return numero in lista
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 alvo = int(input("Digite o número para buscar: "))
 print("Encontrado?", buscar_numero(alvo, numeros))
