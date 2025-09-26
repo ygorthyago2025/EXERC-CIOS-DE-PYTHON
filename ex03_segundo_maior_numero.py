aaada20b17a49b954cda5b73d70b69addc9a3b5e
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:54:17 -0300

    Update ex03_segundo_maior_numero.py

--- ex03_segundo_maior_numero.py
+++ ex03_segundo_maior_numero.py
@@ -1,5 +1,8 @@
 def segundo_maior(lista):
     return sorted(set(lista))[-2]
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 print("Segundo maior número:", segundo_maior(numeros))
