1ae2a8052f04ed26fd2103cfc47c692fd3434a90
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:55:05 -0300

    Update ex04_numeros_pares.py

--- ex04_numeros_pares.py
+++ ex04_numeros_pares.py
@@ -1,5 +1,8 @@
 def numeros_pares(lista):
     return [n for n in lista if n % 2 == 0]
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 print("Números pares:", numeros_pares(numeros))
