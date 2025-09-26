9243d45e49dd99f4a26612ac849d511802422d7d
YGOR THYAGO PEREIRA SILVA <ygorthyago.2007@gmail.com>
2025-09-26 10:55:36 -0300

    Update ex05_numeros_impares.py

--- ex05_numeros_impares.py
+++ ex05_numeros_impares.py
@@ -1,5 +1,9 @@
 def numeros_impares(lista):
     return [n for n in lista if n % 2 != 0]
 
-numeros = list(map(int, input("Digite números separados por espaço: ").split()))
+# Lista fixa
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42,
+           999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2]
+
 print("Números ímpares:", numeros_impares(numeros))
+
